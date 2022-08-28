c51867f7c46240d438234b8aa311d959cfc50fc4
czechmate48 <x>
2022-08-28 15:31:59 -0400

    Created Get-MS365ActiveAuthenticationMethods

--- ConnectToGraph.ps1
+++ ConnectToGraph.ps1
@@ -1,13 +1,50 @@
+######################
+# GATHER INFORMATION #
+######################
+
 Write-Host ""
 Write-Host "This module requires access to the Microsoft Graph. Please enter your Tenant ID and Powershell AppClient ID" -ForegroundColor Yellow
 Write-Host ""
-$TenantId = Read-Host "Tenant ID"
-$AppClientId = Read-Host "AppClientID"
+$Global:TenantId = Read-Host "Tenant ID"
+$Global:AppClientId = Read-Host "AppClientID"
+
+##################################
+# INSTALL MICROSOFT GRAPH MODULE #
+##################################
+
+Write-Host ""
+Write-Host "Determining whether Microsoft Graph module is installed" -ForegroundColor Yellow
+
+if (-not (Get-InstalledModule Microsoft.Graph)){
+    Write-Host "Microsoft Graph module not found" -ForegroundColor Yellow
+    Write-Host "Microsoft Graph module is required for some commands to work properly" -ForegroundColor Yellow
+    $installGraph = Read-Host "Do you want to install the Microsoft Graph module? [Y/N]"
+} else {
+    Write-Host "Microsoft Graph module found" -ForegroundColor Yellow
+}
+
+if ($installGraph -match "[yY]"){
+    
+    Try {
+        Write-Host ""
+        Write-Host "Installing the Microsoft Graph module" -ForegroundColor Yellow
+        Install-Module Microsoft.Graph -Repository PSGallery -Scope CurrentUser -AllowClobber -force -ErrorAction Stop
+        Write-Host "Microsoft Graph module installed successfully" -ForegroundColor Yellow
+    } Catch {
+        Write-Error "Unable to install the Microsoft Graph module"
+        Write-Host "Some commands will not work properly." -ForegroundColor Yellow
+    }
+
+}
+
+##############################
+# CONNECT TO MICROSOFT GRAPH #
+##############################
 
 $MsalParams = @{
     ClientId = $AppClientId
     TenantId = $TenantId
-    Scopes   = "https://graph.microsoft.com/User.Read.All","https://graph.microsoft.com/AuditLog.Read.All"
+    Scopes   = "https://graph.microsoft.com/User.Read.All","https://graph.microsoft.com/AuditLog.Read.All","https://graph.microsoft.com/UserAuthenticationMethod.Read.All"
 }
 
 Try {
@@ -15,7 +52,7 @@ Try {
     Write-Host "Generating access token for Microsoft Graph" -ForegroundColor Yellow
     $MsalResponse = Get-MsalToken @MsalParams -ErrorAction Stop
     $Global:AccessToken  = $MsalResponse.AccessToken
-    $Global:headers = @{'Content-Type'="application\json";'Authorization'="Bearer $AccessToken"}
+    $Global:Headers = @{'Content-Type'="application\json";'Authorization'="Bearer $AccessToken"}
     Write-Host "Access token successfully created" -ForegroundColor Green
     Write-Host "The module is now available" -ForegroundColor Yellow
     Write-Host ""
---
+++ Production/Get-MS365ActiveAuthenticationMethod.ps1
@@ -0,0 +1,90 @@
+Function Get-MS365ActiveAuthenticationMethod {
+
+    <#
+    .SYNOPSIS
+    This cmdlet identifies which authentication methods are active on the user accounts
+    .DESCRIPTION
+    This cmdlet queries the Microsoft Graph to obtain a list of users in the tenant and their associated authentication methods. Any authentications methods that may be used
+    for resetting passwords, logging into the graph, or performing MFA are marked as 'Active'
+    .PARAMETER $UserPrincipalName
+    The user to perform the query against. If no user is specified, all users in the tenant are queried. 
+    .EXAMPLE
+    Get-MS365ActiveAuthenticationMethods -Userprincipalname John.Doe@contoso.com
+    .NOTES
+    https://docs.microsoft.com/en-us/graph/api/resources/authenticationmethods-overview?view=graph-rest-1.0
+    https://docs.microsoft.com/en-us/graph/api/authentication-list-methods?view=graph-rest-1.0
+    #>
+
+    [CmdletBinding()]
+    param (
+        [System.Object[]] $UserPrincipalName = @(), # Declare array as empty or values will be added one letter at a time instead of one UPN at a time
+        [Switch] $member,
+        [Switch] $guest
+    )
+
+    BEGIN{
+
+        # Admin must be global administrator, global reader, privileged authentication administrator, or authentication administrator
+
+        if ($UserPrincipalName.Count -eq 0){
+            $ApiUrl = "https://graph.microsoft.com/v1.0/users?`$top=999"
+            $Response = Invoke-RestMethod -Method Get -Uri $apiurl -headers $headers
+
+            foreach ($resp in $response.value){
+                $UserPrincipalName += $resp.userprincipalname
+            }
+        }
+
+    }
+
+    PROCESS {
+
+        foreach ($user in $UserPrincipalName){
+            $response = Invoke-RestMethod -Method Get -uri "https://graph.microsoft.com/v1.0/users/$user/authentication/methods" -headers $headers
+
+            $Email,$Fido,$Authenticator,$Password,$Phone,$SoftwareOath,$TemporaryAccessPass,$WindowsHello = ''
+
+            foreach ($value in $response.value){
+                if ($value.'@odata.type' -like "*emailAuthenticationMethod*"){
+                    $Email = "Active"
+                }
+                if ($value.'@odata.type' -like "*fido2AuthenticationMethod*"){
+                    $Fido = "Active"
+                }
+                if ($value.'@odata.type' -like "*microsoftAuthentcatorAuthenticationMethod*"){
+                    $Authenticator = "Active"
+                }
+                if ($value.'@odata.type' -like  "*passwordAuthenticationMethod*"){
+                    $Password = "Active"
+                }
+                if ($value.'@odata.type' -like  "*PhoneAuthenticationMethod*"){
+                    $Phone = "Active"
+                }
+                if ($value.'@odata.type' -like  "*softwareOathAuthenticationMethod*"){
+                    $SoftwareOath = "Active"
+                }
+                if ($value.'@odata.type' -like  "*temporaryAccessPassAuthenticationMethod*"){
+                    $TemporaryAccessPass = "Active"
+                }
+                if ($value.'@odata.type' -like  "*windowsHelloForBusinessAuthenticationMethod*"){
+                    $WindowsHello = "Active"
+                }
+            }
+
+            [PSCustomObject]@{
+                UserPrincipalName = $user
+                Email = $Email
+                Fido = $Fido
+                Authenticator = $Authenticator
+                Password = $Password
+                Phone = $Phone
+                SoftwareOath = $SoftwareOath
+                TemporaryAccessPass = $TemporaryAccessPass
+                WindowsHello = $WindowsHello
+            }
+        }
+    
+    }
+
+    END {}
+}
